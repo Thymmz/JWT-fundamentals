19862ad4d6783e034e13f7f2076443e55b695a31
Thymmz <atharva.deshpande228@gmail.com>
2022-02-16 12:37:11 -0500

    completed main functionality

--- controllers/main.js
+++ controllers/main.js
@@ -15,9 +15,22 @@ const login = async (req,res)=>{
 }
 
 const dashboard = async (req, res)=>{
-   const luckyNumber = Math.floor(Math.random()*100)
-   res.status(200).json({msg:`Hello, John Doe`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
-   //res.send('Dashboard here')
+   const authHeader = req.headers.authorization;
+
+   if(!authHeader || !authHeader.startsWith('Bearer ')){
+      throw new CustomAPIError('No Token provide', 400)
+   }
+
+   const token = authHeader.split(' ')[1]
+   try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      const luckyNumber = Math.floor(Math.random()*100)
+      res.status(200).json({msg:`Hello, ${decoded.username}`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+      //res.send('Dashboard here')
+      //console.log(decoded);
+   } catch (error) {
+      throw new CustomAPIError('Not authorized to access this route',401)
+   }
 }
 
 module.exports = {
